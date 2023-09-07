430ba0f1e9de7e0b7c8b2c0cae909d6cb828af3b
GITZMBE <lucasfilip@hotmail.se>
2023-09-07 19:38:11 +0200

    deleted context provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Layer from './pages/Layer';
-import MoreInfo from './pages/MoreInfo';
-import { MovieProvider } from './utils/MovieContext';
-import Filter from './pages/Filter';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layer from "./pages/Layer";
+import MoreInfo from "./pages/MoreInfo";
+import Filter from "./pages/Filter";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
-        <MovieProvider>
-          <Routes>
-            <Route path='/' element={<Layer />}>
-              <Route index element={<Home />} />
-              <Route path='home' element={<Home />} />
-              <Route path='moreInfo' element={<MoreInfo />} />
-              <Route path='filter' element={<Filter />} />
-            </Route>
-          </Routes>        
-        </MovieProvider>
+        <Routes>
+          <Route path='/' element={<Layer />}>
+            <Route index element={<Home />} />
+            <Route path='home' element={<Home />} />
+            <Route path='moreInfo' element={<MoreInfo />} />
+            <Route path='filter' element={<Filter />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
